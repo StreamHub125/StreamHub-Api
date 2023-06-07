c9f56065baec85d3947c08414771faf398f18beb
Daniel Steven Campaz Ocoro <86497840+DanielCampaz@users.noreply.github.com>
2023-06-06 21:58:53 -0500

    Sin Builds

--- build/controllers/Models/ModelRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const Model_1 = require("./Model");
-const ModelRouter = (0, express_1.Router)();
-ModelRouter.get(Model_1.modelController.pathGetModels, Model_1.modelController.getModels);
-exports.default = ModelRouter;
